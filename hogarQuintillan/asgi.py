19dda5f890552c505b967c8479f541ad5900c795
unknown <alberto.bosch.vigo@gmail.com>
2020-04-11 14:12:48 +0200

    fix to launch in heroku

--- hogarQuintillan/asgi.py
+++ hogarQuintillan/asgi.py
@@ -8,12 +8,15 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
 
 import os
+import django
 
-from hogarQuintillan.websocket import websocket_application
-from django.core.asgi import get_asgi_application
+#from hogarQuintillan.websocket import websocket_application
+#from django.core.asgi import get_asgi_application
+from channels.routing import get_default_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hogarQuintillan.settings')
 
+"""
 django_application = get_asgi_application()
 
 async def application(scope, receive, send):
@@ -25,3 +28,7 @@ async def application(scope, receive, send):
         await websocket_application(scope, receive, send)
     else:
         raise NotImplementedError(f"Unknown scope type {scope['type']}")
+
+"""
+django.setup()
+application = get_default_application()
