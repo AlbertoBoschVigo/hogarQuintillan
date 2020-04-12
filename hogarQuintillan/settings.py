8c2274584c9362a5c095f9a2ea8d94e2ca7e0de5
unknown <alberto.bosch.vigo@gmail.com>
2020-04-12 20:59:52 +0200

    ttl conexiones

--- hogarQuintillan/settings.py
+++ hogarQuintillan/settings.py
@@ -156,9 +156,7 @@ if DEBUG:
         }
     }
 else:
-    DATABASES = {
-        'default': os.environ['DATABASE_URL']
-    }
+    DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
